4a08c1382c9513b04e5d3a044696f9fadb8a9367
Connor <cwiegmink@outlook.com>
2020-09-21 23:40:58 +1000

    - Implemented 'compare against previous scores' graph using temporary data - Shifted the layout a bit so the graph will be less short and wide

--- js/site.js
+++ js/site.js
@@ -8,6 +8,17 @@ const LOINC_ANSWER_CODE_SCORES = {
 	"LA6154-4": 5
 }
 
+// Chart.js plugin to set chart area background color. Retrieved from https://github.com/chartjs/Chart.js/issues/3479
+Chart.plugins.register({
+	beforeDraw: function (chartInstance, easing) {
+		var ctx = chartInstance.chart.ctx;
+		ctx.fillStyle = '#fff'; // Set background colour here
+
+		var chartArea = chartInstance.chartArea;
+		ctx.fillRect(chartArea.left, chartArea.top, chartArea.right - chartArea.left, chartArea.bottom - chartArea.top);
+	}
+});
+
 function getk10WordScore(score) {
 	if (score < 20) {
 		return "good";
@@ -16,7 +27,7 @@ function getk10WordScore(score) {
 	} else if (score < 30) {
 		return "moderate";
 	} else {
-		return "severe";	
+		return "severe";
 	}
 };
 
@@ -47,7 +58,7 @@ function createQuestionaire(data) {
 	// var type = ["radio", "text"];
 
 	var questionsAmount = data.item[0].item.length;
-	
+
 	for (var k = 0; k < questionsAmount; k++) {
 
 		var answers = "";
@@ -69,9 +80,9 @@ function createQuestionaire(data) {
 
 			answers = answers + answer;
 		}
-		
+
 		var name = data.item[0].item[k].text;
-		
+
 		var question = `<p>${name}</p>${answers}<br>`;
 		$("#questionaire").append(question);
 	}
@@ -90,12 +101,12 @@ function setScoreScale(score) {
 	// Also transform it left by this percentage of its own width so it never goes over the edges of the scale
 	//		(e.g. a score of 100% will transform the marker left 100% of its own width)
 	$("#overall-score-container").css({
-		"left": scorePercent + "%", 
+		"left": scorePercent + "%",
 		"transform": "translateX(-" + scorePercent + "%)"
 	});
 	// Set the score value
 	$("#score-value").text(score);
-} 
+}
 function setNormativeScoreScale(score) {
 	// Convert score to a percentage of max possible score (setting the minimum to zero rather than ten)
 	var scorePercent = ((score - 10) / 40) * 100;
@@ -133,6 +144,63 @@ function setNextSteps(score) {
 	$(".next-steps-" + getk10WordScore(score)).show();
 }
 
+function handlePreviousScores(currentScore) {
+	// TODO: implement properly
+	var dateArray = ["2019-06-24", "2020-08-16 (current)"];
+	var scoreArray = [29, currentScore];
+	// Create array of background colors for the bars so the current bar can be a different colour
+	var barColors = [];
+	// TODO: Change this to loop through all past questionnaires, and set the **CURRENT** questionnaire as the other colour NOT just the last one (cause might be viewing a past questionnaire)
+	dateArray.forEach(function(val, index) {
+		if (index === (dateArray.length - 1)) {
+			barColors.push("#0573a4");
+		} else {
+			barColors.push("#043a5e");
+		}
+	});
+
+	var graphContext = document.getElementById("previous-scores-graph").getContext("2d");
+	var graph = new Chart(graphContext, {
+		type: "bar",
+		data: {
+			labels: dateArray,
+			datasets: [{
+				backgroundColor: "#043a5e",
+				data: scoreArray,
+				backgroundColor: barColors ? barColors : "#043a5e"
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			/*aspectRatio: 1.5,*/
+			scales: {
+				yAxes: [{
+					type: "linear",
+					ticks: {
+						min: 0,
+						max: 50,
+						fontColor: "#ffffff"
+					},
+					gridLines: {
+						display: false
+					}
+				}],
+				xAxes: [{
+					ticks: {
+						fontColor: "#ffffff"
+					},
+					gridLines: {
+						display: false
+					}
+				}]
+			},
+			legend: {
+				display: false
+			}
+		}
+	});
+}
+
 function handleQuestionnaireResponse(responseJson) {
 	var scores = [];
 	// Get scores for each answer
@@ -140,21 +208,22 @@ function handleQuestionnaireResponse(responseJson) {
 		item = responseJson.item[i];
 		var score = getAnswerScore(item.answer);
 		if (score > 0) {
-			console.log("Score of " + score + " for question " + item.text);
 			scores.push(score);
 		}
 	}
-	
+
 	// Calculate and display total score
 	var totalScore = scores.reduce((total, value) => total + value);
-	console.log("Total score = " + totalScore + " '" + getk10WordScore(totalScore) + "'");
 	setScoreScale(totalScore);
-	
+
 	// Set relevant data analysis
 	setDataAnalysis(totalScore);
-	
+
 	// Set relevant next steps
 	setNextSteps(totalScore);
+
+	// Handle Previous Scores
+	handlePreviousScores(totalScore);
 }
 
 function display(data) {
@@ -163,42 +232,36 @@ function display(data) {
 	createQuestionaire(data);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	$("#whatever").text("Loading...");
-	
+
 	// Set comparison data
 	setNormativeScoreScale(14);
-	
+
 	const client = new FHIR.client("http://hapi.fhir.org/baseR4");
 	data = client.request("Questionnaire/MDS3.0-SP-1.14")
-	.then(display)
-	.catch(display);
-	
+		.then(display)
+		.catch(display);
+
 	// Handle This Questionnaire Response (for now just use example)
 	$.ajax({
 		url: "/testResources/k10-questionnaire-response.json",
 		type: "GET",
-		success: function(data) {
+		success: function (data) {
 			handleQuestionnaireResponse(data);
 		}
 	});
-	
-	// Handle Previous Scores
-	// TODO: implement and move this to a function
-	if (/* as past scores*/false) {
-	} else {
-		$("#previous-scores").text("This person has not completed any other K10 questionnaires.");		
-	}
 
-	$("#questionaire").submit(function( event ) {
+
+	$("#questionaire").submit(function (event) {
 		$.ajax({
 			type: "POST",
 			url: "serverUrl",
 			data: formData,
-			success: function(){},
+			success: function () { },
 			dataType: "json",
-			contentType : "application/json"
+			contentType: "application/json"
 		});
 
 
