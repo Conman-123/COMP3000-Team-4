bbe61fa2e230d0bea4a65f4207633db1a28ac40b
Victor Ngo <tminhngvt1998@gmail.com>
2020-09-22 12:57:45 +1000

    updated questionaireDisplay

--- js/site.js
+++ js/site.js
@@ -57,39 +57,44 @@ function questionInput(type, value, name) {
 function createQuestionaire(data) {
 	// var type = ["radio", "text"];
 
-	var questionsAmount = data.item[0].item.length;
+	stack = data.reverse();
+	n = 0
 
-	for (var k = 0; k < questionsAmount; k++) {
+	while (stack.length > 0) {
+		questionData = stack.pop();
+		console.log(n+= 1);
 
-		var answers = "";
-		var answersAmount;
-		var input;
-		var answer;
-
-		if (data.item[0].item[k].type == "group") {
-			var furtherQuestions = data.item[0].item[k].item;
-			continue;
+		if (questionData.type == "group") {
+			if (questionData.hasOwnProperty("item")) {
+				stack = stack.concat(questionData.item.reverse());
+			}
 		} else {
-			answersAmount = data.item[0].item[k].answerOption.length;
+			console.log(questionData);
+			if (questionData.type == "choice") {
+				var answers = "";
+				var answersAmount;
+				var input;
+				var answer;
+
+				for (var i = 0; i < questionData.answerOption.length; i++) {
+
+					input = questionInput(questionData.type, questionData.answerOption[i].code, questionData.text);
+					answer = `<label>${input + questionData.answerOption[i].valueCoding.display}</label><br>`;
+
+					answers = answers + answer;
+				}
+
+				var name = questionData.text;
+
+				var question = `<p>${name}</p>${answers}<br>`;
+				$("#questionaire").append(question);
+			}			
 		}
 
-		for (var i = 0; i < answersAmount; i++) {
-
-			input = questionInput(data.item[0].item[k].type, data.item[0].item[k].answerOption[i].code, data.item[0].item[k].text);
-			answer = `<label>${input + data.item[0].item[k].answerOption[i].valueCoding.display}</label><br>`;
-
-			answers = answers + answer;
-		}
-
-		var name = data.item[0].item[k].text;
-
-		var question = `<p>${name}</p>${answers}<br>`;
-		$("#questionaire").append(question);
 	}
 
 	$("#questionaire").append('<input type="submit" formtarget="_self">');
 }
-
 function displayQuestionnaireResponse(responseItems) {
 	// TODO: display the responses 
 }
@@ -229,7 +234,7 @@ function handleQuestionnaireResponse(responseJson) {
 function display(data) {
 	console.log(data);
 	//$("#whatever").text(data instanceof Error ? String(data) : JSON.stringify(data, null, 4));
-	createQuestionaire(data);
+	createQuestionaire(data.item);
 }
 
 $(document).ready(function () {
