335d759fad179c79d0357d71a0462d9da0fbcda4
Victor Ngo <tminhngvt1998@gmail.com>
2020-10-06 14:08:06 +1000

    added questionaire generation

---
+++ js/questionnaireDisplay.js
@@ -0,0 +1,139 @@
+function shortenName(name) {
+	var nameRE = /((?:\w+)(?:\/|\s)?)+/g;
+	return name.match(nameRE)[0];
+}
+
+function questionInput(type, value, name) {
+
+	var input = "";
+	name = shortenName(name);
+
+	if (type == "choice" || type == "open-choice") {
+		input = `<input value="${value}" name="${name}" type="radio">`;
+		return input;
+	}
+
+	if (type == "decimal" || type == "integer") {
+		input = `<input name="${name}" type="number">`;
+		return input;
+	}
+
+	return '<input type="radio">';
+}
+
+function displayQuestionnaireResponse(responseItems) {
+	// TODO: display the responses 
+}
+
+var questions = [];
+
+function getQuestionData(data) {
+	// var type = ["radio", "text"];
+
+	stack = data.reverse();
+	n = 0
+
+	while (stack.length > 0) {
+		questionData = stack.pop();
+		console.log(n+= 1);
+
+		if (questionData.type == "group") {
+			if (questionData.hasOwnProperty("item")) {
+				stack = stack.concat(questionData.item.reverse());
+			}
+		} else {
+			console.log(questionData);
+			var question = [
+				questionData.linkId,
+				questionData.prefix,
+				questionData.text,
+				questionData.type,
+				questionData.answerOption
+			];
+
+			questions.push(question);
+		}
+	}
+
+	return questions;
+}
+
+function getPrefix(question) {
+	return question[1];
+}
+
+function getDisplayText(question) {
+	return question[2];
+}
+
+function getAnswers(question) {
+	return question[4];
+}
+
+function getValue(answer) {
+	return answer.valueCoding.code;
+}
+
+function createAnswerId(answer, name) {
+	var re = /(?:\w+)/g;
+	var id = answer.match(re)[0];
+	return `${name}-${id}`;
+}
+
+function createAnswerName(question) {
+	return `question-${question[0]}`
+}
+
+function displayQuestionaire(quesions) {
+	console.log("run");
+	$("#questionaire").append(`
+			<div class="row questionnaire-radio-group-label-row">
+				<div class="col-8 offset-4 answer-col" id="colLabel">
+				</div>
+			</div>
+		`);
+
+	var question = questions[0];
+	for (var i = 0; i < 5; i++) {
+		var label = question[4][i].valueCoding.display;
+
+		$("#colLabel").append(`
+			<span class="radio-label">${label}</span>
+			`);
+	}
+
+	for (var i = 0; i < questions.length; i++) {
+		var prefix = getPrefix(questions[i]);
+		var display = getDisplayText(questions[i]);
+		var name = createAnswerName(questions[i]);
+		var answers = getAnswers(quesions[i]);
+		var answersText = "";
+
+		for (var k = 0; k < answers.length; k++) {
+			var value = getValue(answers[k]);
+			var text = answers[k].valueCoding.display;
+			var id = createAnswerId(text, name);
+
+			answersText += `
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" id="${id}" name="${name}" type="radio" value="${value}">
+					<label class="form-check-label sr-only" for="${id}">${text}</label>
+				</div>
+			`;
+		}
+		var questionContainer = `
+				<div class="row questionnaire-row">
+					<div class="col-4 question-col">
+						${prefix} ${display}
+					</div>
+					<div class="col-8 answer-col">
+						${answersText}
+					</div>
+				</div>
+			`;
+
+		$("#questionaire").append(questionContainer);
+	}
+
+	$("#questionaire").append(`<button type="submit" form="questionaire" value="Submit">Submit</button>`);
+}
